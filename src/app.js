fbfa8e1e03b1a6620d7fa43bf284d74e4b0dd944
Rodrigo Viturino <rodrigo.souza@spread.com.br>
2021-05-20 19:46:24 -0300

    Criação do método de GET, POST E PUT

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const uuid = require("uuid");
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -11,15 +12,49 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json({repositories});
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repos = {
+    id: uuid.v4(),
+    title,
+    url,
+    techs,
+    likes : 0
+  };
+
+  repositories.push(repos);
+
+  return response.status(201).json({repos});
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const idRepository = repositories.findIndex((repository) => repository.id === id);
+
+  if(idRepository === -1){
+    return response.status(400).json({
+      message: "Repository not exists!"
+    });
+  };
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[idRepository].likes
+  };
+
+  repositories[idRepository] = repository;
+
+  return response.status(200).json({repository});
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
